8c3193820270cefb3b1855de254a3e596882e0c5
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-04-03 15:55:56 -0400

    set custom content-grid breakpoints

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -6,17 +6,22 @@ import { kebabCase } from "lodash"
 export default function PostCard({ post }) {
   return (
     <div key={post.id} className="card">
-      <Img
-        fluid={post.frontmatter.featuredImage.childImageSharp.fluid}
-        className="card-image"
-      />
-      <Link className="nav-link" to={post.fields.slug}>
-        <h2 className="card-heading title">{post.frontmatter.title}</h2>
-        <p className="card-text">{post.frontmatter.description}</p>
-      </Link>
+      <div>
+        <Img
+          fluid={post.frontmatter.featuredImage.childImageSharp.fluid}
+        />
+        <Link className="nav-link" to={post.fields.slug}>
+          <h2 className="card-heading title">{post.frontmatter.title}</h2>
+          <p className="card-text">{post.frontmatter.description}</p>
+        </Link>
+      </div>
       <nav className="card-heading">
         {post.frontmatter.tags.map(tag => (
-          <Link className="nav-link text--md" to={`/posts/${kebabCase(tag)}`}>
+          <Link
+            className="nav-link text--md"
+            to={`/posts/${kebabCase(tag)}`}
+            key={tag}
+          >
             {tag}
           </Link>
         ))}
