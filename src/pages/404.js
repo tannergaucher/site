06e2f6eabffd6810eaa7d3b4a677cf8f522daff8
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-04-21 20:20:48 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import React from "react"
 const NotFoundPage = ({ location }) => (
   <Layout location={location}>
     <SEO title="404: Not found" />
-    <div className="page padding">
+    <div className="container page padding">
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
